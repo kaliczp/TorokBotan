d160814b0c019a1e59828851462ce32719f9ba93
Kalicz Péter <kaliczp@gmail.com>
2024-10-09 22:02:33 +0200

    Clip with polygon

--- Process.R
+++ Process.R
@@ -13,6 +13,21 @@ plot(las, color = "RGB")
 subset1 <- clip_rectangle(las, 618250, 5281925, 618334, 5282000)
 plot(subset1, color = "RGB")
 
+subset2 <- clip_polygon(las, xpoly = c(618325,
+                                       618343,
+                                       618280,
+                                       618265,
+                                       618325
+                                       ),
+                        ypoly = c(5281920,
+                                  5281981,
+                                  5282003,
+                                  5281940,
+                                  5281920
+                                  )
+                        )
+
+
 ## Canopy
 chm <- rasterize_canopy(las, 0.5, pitfree(subcircle = 0.2))
 ## Tree tops
