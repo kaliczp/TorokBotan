3517e6d1cb83bde5ad1ce0261998430e5712fd02
Kalicz Péter <kaliczp@gmail.com>
2024-10-09 18:52:24 +0200

    Some explanation and installation instructions

--- Process.R
+++ Process.R
@@ -1,3 +1,8 @@
+## R honlap : https://www.r-project.org/
+## A program letöltése CRAN ról
+install.packages("lidR")
+## Csomag betöltése
 library(lidR)
+## Fájl beolvasás és jellemzők
 las <- readLAS("lidars/terra_las/cloud619adf4281ddc50.las")
 las
